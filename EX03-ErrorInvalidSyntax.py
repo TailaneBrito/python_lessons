3bac1a1d5c42e4b6a8bd2ea517cd32fc9098c4d1
TailaneBrito <tailane.brito01@gmail.com>
2017-08-17 22:27:18 -0300

    EX03-ErrorInvalidSyntax.py

---
+++ EX03-ErrorInvalidSyntax.py
@@ -0,0 +1,9 @@
+text="this's a test with an error";
+print(text);
+
+text2='this"s a text with an error 2';
+print(text2);
+
+#esse erro acontece pq falta fazer o escape de
+#aspas. eh ckmum em varias outras linguagens
+#de programação
