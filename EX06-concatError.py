a023cb96d3e324e6e4e3869606ff0a5817ae72b3
TailaneBrito <tailane.brito01@gmail.com>
2017-08-17 22:34:18 -0300

    EX06-concatError.py

---
+++ EX06-concatError.py
@@ -0,0 +1,12 @@
+monthBirth="May"
+person="Tailane"
+year=2017
+yo=22
+
+print ("Hello" + person + " you are" +
+yo + " on " year)
+
+#esse erro acontece porque o python tenta
+#fazer operacoes aritmeticas juntando
+#variaveis tipo string com inteiros/double
+#se for o caso.
