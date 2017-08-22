e49108b6dd4d6c8065ffe33a55e39d65610bd50b
TailaneBrito <tailane.brito01@gmail.com>
2017-08-22 03:15:28 -0300

    EX10-LastListValue.py

---
+++ EX10-LastListValue.py
@@ -0,0 +1,9 @@
+compras = ['tomate','queijo','suco']
+print (compras [-1])
+
+#desse modo o python vai retornar recursivamente
+#os valoes sortados da direita para a esquerda
+
+#o numero negativo deve estar sempre com o valor
+#total dos itens na lista. e o ultimo valor
+#sera apresentado de forma negativa
