30b3779dff5fe33425e40056e2038f9d3ee1d65c
TailaneBrito <tailane.brito01@gmail.com>
2017-08-21 23:27:31 -0300

    EX08-ListPosition.py

---
+++ EX08-ListPosition.py
@@ -0,0 +1,7 @@
+friends = ['Kelli','Greicy','Bianca']
+best_friend = friends[2]
+print(best_friend)
+
+#a declaração de lista começa na posição 0
+#passando a posição 2 eu terei a terceira ocorrenica
+#da minha lista
